815c06df8b6ba155b91a3c94b9acbe52397378bf
danielmir2 <daniel@lesens.fr>
2021-11-19 11:26:03 +0100

    click

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 window.onload = () => {
+    // On récupère toutes les balises details
+    let details = document.querySelectorAll("details");
 
-    // ### code ok ###
-    let detailsAll = document.querySelectorAll("details");
-
-    for (let details of detailsAll) {
-
-        details.addEventListener("mouseover", function () {
-            details.setAttribute("open", true);
+    for(let detail of details){
+        // On écoute l'évènement "mouseover"
+        detail.addEventListener("mouseover", function(){
+            // On ajoute l'attribut "open" au "detail" survolé
+            this.setAttribute("open", "");
         });
-
-        details.addEventListener("mouseout", function () {
-            details.removeAttribute("open");
+        // On écoute l'évènement "mouseout"
+        detail.addEventListener("mouseout", function(){
+            // On retire l'attribut "open" au "detail" plus survolé
+            this.removeAttribute("open");
         });
-
+        // On écoute l'évènement "click"
+        detail.addEventListener("click", function(e){
+            // On boucle sur tous les éléments details
+            for(let detail2 of details){
+                // On vérifie que detail2 est différent de this (le detail cliqué)
+                if(this !== detail2){
+                    detail2.removeAttribute("open");
+                }
+            }
+        });
     }
 }
